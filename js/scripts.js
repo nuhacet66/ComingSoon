a8ecf704e787c43ee4a8136d5b6d838a6594f770
Nuhacet <nuhacetDeveloper@gmail.com>
2024-03-17 13:09:02 +0000

    documents for development proyects

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const sr = ScrollReveal({
+    distance: '90px',
+    duration: 3000,
+})
+
+sr.reveal('.home_data', {origin: 'top', delay: 400})
+sr.reveal('.home_img', {origin: 'bottom', delay: 600})
